51b9f46550a8790043414e22db5b8e9fbf683928
Andrey532 <andreygalushka30@gmail.com>
2023-08-05 14:36:41 +0300

    first schetchik

---
+++ my-app/src/Counter.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import styles from "./Counter.module.css";
+
+export const Counter = () => {
+  const [value, setValue] = useState(0);
+  const onClickInc = () => {
+    setValue(value + 1);
+    if (value > 6) {
+      setValue(7);
+    }
+  };
+  const onClickReset = () => {
+    setValue(0);
+  };
+  return (
+    <div className={styles.main}>
+      <div className={styles.num}>
+        <span className={value === 7 ? styles.maxValCol : ""}>{value}</span>
+      </div>
+      <div className={styles.btns}>
+        <button className={styles.btnInc} onClick={onClickInc}>
+          inc
+        </button>
+        <button className={styles.btnReset} onClick={onClickReset}>
+          reset
+        </button>
+      </div>
+    </div>
+  );
+};
